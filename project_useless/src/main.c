922bec70333a6faee26ffb9667aa64177e8f7314
marvers <baptiste.renneson@ecole-89.com>
2021-04-28 15:41:15 +0200

    update !

--- project_useless/src/main.c
+++ project_useless/src/main.c
@@ -3,6 +3,12 @@
 int main (void)
 {
   while (1)
-  	printf("%c", rand()%127);
+    {
+      for (int i = 0; i < 100000; ++i)
+	{
+	  printf("%c", rand()%127);
+	}
+      sleep(1);
+    }
   return 0;
 }
